2cb24c35e7ada6e187dedd160955b8ddddc56480
lwang114 <lwang114@illinois.edu>
2019-06-30 08:59:49 -0500

    run_acoustic_wordseg memory efficiency

--- comparison_models/run_acoustic_wordseg.py
+++ comparison_models/run_acoustic_wordseg.py
@@ -39,7 +39,7 @@ def downsample(y, n, args):
   return y_new
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--embed_dim", type=int, default=300, help="Dimension of the embedding vector")
+parser.add_argument("--embed_dim", type=int, default=180, help="Dimension of the embedding vector")
 parser.add_argument("--n_slices_min", type=int, default=10, help="Minimum length of the segment")
 parser.add_argument("--n_slices_max", type=int, default=40, help="Maximum length of the segment")
 parser.add_argument("--technique", choices={"resample", "interpolate", "rasanen"}, default="resample", help="Downsampling technique")
@@ -49,7 +49,7 @@ parser.add_argument("--exp_dir", type=str, default='./', help="Experimental dire
 args = parser.parse_args()
 print(args)
 
-datapath = "../data/flickr30k/audio_level/flickr_bnf_subset.npz"
+datapath = "../data/flickr30k/audio_level/flickr_bnf_all_src.npz"
 # Generate acoustic embeddings, vec_ids_dict and durations_dict 
 audio_feats = np.load(datapath)
 embedding_mats = {}
@@ -57,7 +57,7 @@ vec_ids_dict = {}
 durations_dict = {}
 landmarks_dict = {}
 
-start_step = 0
+start_step = 1
 if start_step == 0:
   print("Start extracting acoustic embeddings")
   begin_time = time.time()
@@ -68,14 +68,14 @@ if start_step == 0:
     feat_dim = feat_mat.shape[1]
     assert args.embed_dim % feat_dim == 0
     assert args.n_slices_min >= args.embed_dim / feat_dim
-    embed_mat = np.zeros((n_slices * (1 + n_slices) / 2, args.embed_dim))
+    embed_mat = np.zeros(((args.n_slices_max - args.n_slices_min + 1)*n_slices, args.embed_dim))
     vec_ids = -1 * np.ones((n_slices * (1 + n_slices) / 2,))
     durations = np.nan * np.ones((n_slices * (1 + n_slices) / 2,))
 
     i_embed = 0        
     # Store the vec_ids using the mapping i_embed = end * (end - 1) / 2 + start (following unigram_acoustic_wordseg.py)
     for cur_start in range(n_slices):
-        for cur_end in range(cur_start + args.n_slices_min, min(n_slices, cur_start + args.n_slices_max)):
+        for cur_end in range(cur_start + args.n_slices_min - 1, min(n_slices, cur_start + args.n_slices_max) - 1):
             cur_end += 1
             t = cur_end
             
@@ -88,24 +88,38 @@ if start_step == 0:
             durations[i + cur_start] = cur_end - cur_start
             i_embed += 1 
 
-
     vec_ids_dict[feat_id] = vec_ids
     embedding_mats[feat_id] = embed_mat
     durations_dict[feat_id] = durations 
     landmarks_dict[feat_id] = np.arange(n_slices).tolist()
   print("Take %0.5f s to finish extracting embedding vectors !" % (time.time()-begin_time))
-  #np.savez("embedding_mats.npz", **embedding_mats)
-  np.savez("vec_ids_dict.npz", **vec_ids_dict)
-  np.savez("durations_dict.npz", **durations_dict)
-  np.savez("landmarks_dict.npz", **landmarks_dict)  
+  np.savez(args.exp_dir+"embedding_mats.npz", **embedding_mats)
+  np.savez(args.exp_dir+"vec_ids_dict.npz", **vec_ids_dict)
+  np.savez(args.exp_dir+"durations_dict.npz", **durations_dict)
+  np.savez(args.exp_dir+"landmarks_dict.npz", **landmarks_dict)  
 
 if start_step <= 1:
   print("Start training segmentation models")
   begin_time = time.time()
-  #embedding_mats = np.load("embedding_mats.npz")
-  #vec_ids_dict = np.load("vec_ids_dict.npz")
-  #durations_dict = np.load("durations_dict.npz")
-  #landmarks_dict = np.load("landmarks_dict.npz")
+  embedding_mats = dict(np.load(args.exp_dir+"embedding_mats.npz"))
+  vec_ids_dict = dict(np.load(args.exp_dir+"vec_ids_dict.npz"))
+  durations_dict = dict(np.load(args.exp_dir+"durations_dict.npz"))
+  landmarks_dict = dict(np.load(args.exp_dir+"landmarks_dict.npz"))
+
+  for embed_id in embedding_mats:
+    if embedding_mats[embed_id].shape[0] > 500500:
+      logging.debug("sentence with id %s is exceedingly long" % embed_id)
+    
+    if embed_id.split("_")[0] == "3652859271":
+      embedding_mats[embed_id] = embedding_mats[embed_id][:1000*(args.n_slices_max-args.n_slices_min)]
+      vec_ids_dict[embed_id] = vec_ids_dict[embed_id][:500500]
+      durations_dict[embed_id] = durations_dict[embed_id][:500500]
+      landmarks_dict[embed_id] = landmarks_dict[embed_id][:1000]
+    
+  np.savez(args.exp_dir+"embedding_mats.npz", **embedding_mats)
+  np.savez(args.exp_dir+"vec_ids_dict.npz", **vec_ids_dict)
+  np.savez(args.exp_dir+"durations_dict.npz", **durations_dict)
+  np.savez(args.exp_dir+"landmarks_dict.npz", **landmarks_dict)  
 
   # Acoustic model parameters
   segmenter = None
