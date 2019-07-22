847bd8fb29827b432e92bc43ea570984a654ad26
lwang114 <lwang114@illinois.edu>
2019-07-22 16:29:42 -0500

    Update plot functions

--- utils/plot.py
+++ utils/plot.py
@@ -1,10 +1,19 @@
 import json
 import os
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve 
 from collections import defaultdict
-from postprocess import _findPhraseFromPhoneme 
+from librosa.display import specshow
+try:
+  from postprocess import _findPhraseFromPhoneme 
+  from clusteval import _findWords
+except:
+  from utils.postprocess import _findPhraseFromPhoneme 
+  from utils.clusteval import _findWords
+
 
 NULL = 'NULL'
 END = '</s>'
@@ -87,14 +96,27 @@ def plot_attention(src_sent, trg_sent, attention, filename=None, title=None,  no
     plt.show()
   plt.close()
 
-def plot_img_concept_distribution(json_file, concept2idx_file, out_file='class_distribution', cutoff=100, draw_plot=True):
+def plot_img_concept_distribution(json_file, concept2idx_file=None, out_file='class_distribution', cutoff=100, draw_plot=True):
   labels = []
   with open(json_file, 'r') as f:
     pair_info = json.load(f)
-
-  with open(concept2idx_file, 'r') as f:
-    class2idx = json.load(f)
   
+  class2idx = {}
+  if concept2idx_file:
+    with open(concept2idx_file, 'r') as f:
+      class2idx = json.load(f)
+  else:
+    i_c = 0
+    for p in pair_info:
+      concepts = p["image_concepts"]
+      for c in concepts:
+        if c not in class2idx:
+          class2idx[c] = i_c
+          i_c += 1
+  
+    with open("concept2idx.json", "w") as f:
+      json.dump(class2idx, f, indent=4, sort_keys=True)
+
   idx2class = sorted(class2idx, key=lambda x:class2idx[x])  
 
   for p in pair_info:
@@ -104,7 +126,7 @@ def plot_img_concept_distribution(json_file, concept2idx_file, out_file='class_d
    
   return plot_class_distribution(labels, idx2class, filename=out_file, cutoff=cutoff, draw_plot=draw_plot)
 
-def plot_word_len_distribution(json_file, out_file='word_len_distribution', cutoff=1000, draw_plot=True):
+def plot_word_len_distribution(json_file, out_file='word_len_distribution', cutoff=1000, draw_plot=True, phone_level=True):
   labels = []
   
   with open(json_file, 'r') as f:
@@ -112,18 +134,29 @@ def plot_word_len_distribution(json_file, out_file='word_len_distribution', cuto
   
   tot = 0
   for p in pair_info:
-    ali = p['alignment']
-    sent = p['caption']
-    concepts = p['image_concepts']
-    phrases, concept_indices = _findPhraseFromPhoneme(sent, ali)
-    for i, p in enumerate(phrases):
-      if concepts[concept_indices[i]] == NULL or concepts[concept_indices[i]] == END:
-        labels.append(len(p))
+    ali = p['alignment']  
+    concepts = sorted(p['image_concepts'])
+      
+    if phone_level:
+      sent = p['caption']
+      phrases, concept_indices = _findPhraseFromPhoneme(sent, ali)
+      for i, ph in enumerate(phrases):
+        if concepts[concept_indices[i]] == NULL or concepts[concept_indices[i]] == END:
+          continue
+        labels.append(len(ph))
         tot += 1
-    
-  if DEBUG:
-    print(tot) 
-         
+    else:  
+      boundaries = _findWords(ali)
+
+      for start, end in boundaries:       
+        if concepts[ali[start]] == NULL or concepts[ali[start]] == END: 
+          continue
+        labels.append(end - start)
+        tot += 1
+
+    if DEBUG:
+      print(tot) 
+      
   max_len = max(labels)  
   len_dist = np.zeros((max_len+1,))   
   for l in labels:
@@ -172,10 +205,14 @@ def generate_smt_alignprob_plots(in_file, indices=None, out_dir=''):
   for index, ali in enumerate(align_info):
     if indices and index not in indices:
       continue
-    sent = ali['caption'] 
     concepts = ali['image_concepts']
     align_prob = np.array(ali['align_probs'])
     normalized = (align_prob.T / np.sum(align_prob, axis=1)).T 
+    if "caption" in ali.keys():
+      sent = ali['caption'] 
+    else:
+      sent = [str(t) for t in range(len(align_prob))]
+
     if DEBUG:
       print(normalized, np.sum(normalized, axis=1))
     plot_attention(sent, concepts, normalized, '%s%s.png' % (out_dir, str(index)))
@@ -239,9 +276,13 @@ def plot_roc(pred_file, gold_file, class_name, out_file=None, draw_plot=True):
 
   y_scores = []
   y_true = []
-  for p, g in zip(pred, gold):
+  if DEBUG:
+    print("len(pred), len(gold): ", len(pred), len(gold))
+
+  for i, (p, g) in enumerate(zip(pred, gold)):
     g_concepts = g['image_concepts'] 
     p_ali, g_ali = p['alignment'], g['alignment']
+
     p_probs = None
     if 'align_probs' in p:
       p_probs = p['align_probs']
@@ -250,7 +291,14 @@ def plot_roc(pred_file, gold_file, class_name, out_file=None, draw_plot=True):
     else:
       raise TypeError('Invalid file format')
 
+    #if DEBUG:
+    #  print("gold image concepts, pred image concepts", g["image_concepts"], p["image_concepts"])
+    #  print("i, index, p_ali.shape, g_ali.shape: ", i, p["index"], np.asarray(p_ali).shape, np.asarray(g_ali).shape)
+    #  print("# of concepts for gold, # of concepts for pred: ", len(g_concepts), np.asarray(p_probs).shape[1])
+
     for a_p, a_g, p_prob in zip(p_ali, g_ali, p_probs):
+      #if DEBUG:
+      #  print("a_p, a_g, p_prob.shape: ", a_p, a_g, np.asarray(p_prob).shape)
       if g_concepts[a_g] == class_name:
         y_true.append(1)
       else:
@@ -277,7 +325,7 @@ def plot_roc(pred_file, gold_file, class_name, out_file=None, draw_plot=True):
   
   return fpr, tpr, thresholds
 
-def plot_avg_roc(pred_json, gold_json, concept2idx='../data/flickr30k/concept2idx.json', freq_cutoff=100, out_file=None):
+def plot_avg_roc(pred_json, gold_json, concept2idx=None, freq_cutoff=100, out_file=None):
   top_classes, top_freqs = plot_img_concept_distribution(gold_json, concept2idx, cutoff=10, draw_plot=False)
   #avg_fpr = 0.
   #avg_tpr = 0.
@@ -301,11 +349,37 @@ def plot_avg_roc(pred_json, gold_json, concept2idx='../data/flickr30k/concept2id
   plt.close()
   #avg_fpr += fpr
   #avg_tpr += tpr
+
+def plot_acoustic_features(utterance_idx, audio_dir, feat_dir, out_file=None):
+  mfccs = np.load(feat_dir+"flickr_mfcc_cmvn.npz", "r")
+  bnfs = np.load(feat_dir+"flickr_bnf_all_src.npz", "r")
+  mfcc_keys = sorted(mfccs, key=lambda x:int(x.split('_')[-1]))
+  bnf_keys = sorted(bnfs, key=lambda x:int(x.split('_')[-1]))
   
+  mfcc = mfccs[mfcc_keys[utterance_idx]]
+  bnf = bnfs[bnf_keys[utterance_idx]]
+  
+  plt.figure(figsize=(12, 8))
+  plt.subplot(2, 1, 1)
+  specshow(mfcc, y_axis="linear")
+  plt.colorbar(format="%+2.0f dB")
+  plt.title("MFCC of %s" % mfcc_keys[utterance_idx])
+  
+  plt.subplot(2, 1, 2)
+  specshow(bnf, y_axis="linear")
+  plt.colorbar(format="%+2.0f dB")
+  plt.title("Bottleneck feature of %s" % bnf_keys[utterance_idx])
+
+  if out_file:
+    plt.savefig(out_file)
+  else:
+    plt.show()
+  plt.close()
+
 if __name__ == '__main__':
-  labels = []
+  '''labels = []
   #top_classes, _ = plot_img_concept_distribution('../data/flickr30k/phoneme_level/flickr30k_gold_alignment.json', '../data/flickr30k/concept2idx.json', cutoff=30)
-  '''
+  
   fig, ax = plt.subplots(figsize=(15, 10))
   top_classes, top_freqs = plot_word_len_distribution('../data/flickr30k/phoneme_level/flickr30k_gold_alignment.json', draw_plot=False)
   plt.plot(top_classes[:50], top_freqs[:50])
@@ -338,7 +412,7 @@ if __name__ == '__main__':
   plt.legend(labels, fontsize=30)  
   plt.savefig('word_len_compare.png')
   plt.close()
-  '''
+  
   exp_dir = '../../status_report_mar8th/outputs/samples/'
   pred_json = '../smt/exp/ibm1_phoneme_level_clustering/flickr30k_pred_alignment.json'
   #'../nmt/exp/feb26_normalize_over_time/output/alignment.json'
@@ -357,4 +431,9 @@ if __name__ == '__main__':
   generate_smt_alignprob_plots(pred_json, indices, 'smt_')
   generate_gold_alignment_plots(gold_json, indices, 'gold_')
   
-  #plot_avg_roc(pred_json, gold_json, concept2idx='../data/flickr30k/concept2idx.json', out_file='nmt_roc')
+  #plot_avg_roc(pred_json, gold_json, concept2idx='../data/flickr30k/concept2idx.json', out_file='nmt_roc')'''
+
+  audio_dir = "/home/lwang114/data/flickr_audio/wavs/"
+  feat_dir = "../data/flickr30k/audio_level/"
+  utterance_idx = 0
+  plot_acoustic_features(utterance_idx, audio_dir, feat_dir, "%d_features" % utterance_idx)
