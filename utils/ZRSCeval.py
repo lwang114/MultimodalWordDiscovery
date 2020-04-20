c829f1a234dbf556c2e630982351fde2577c8501
lwang114 <lwang114@illinois.edu>
2020-04-20 14:53:26 -0500

    update ZRSCeval

--- utils/ZRSCeval.py
+++ utils/ZRSCeval.py
@@ -6,28 +6,38 @@ from WDE.measures.coverage import *
 from WDE.measures.boundary import *
 from WDE.measures.ned import *
 from WDE.measures.token_type import *
-from preprocess import *
+from postprocess import *
+import argparse
 
-# XXX
+parser = argparse.ArgumentParser()
+parser.add_argument('--exp_dir', type=str, default='./', help='Experimental directory containing the alignment files')
+parser.add_argument('--dataset', choices=['mscoco2k', 'mscoco20k', 'flickr'])
+args = parser.parse_args()
+
 tasks = [2]
 #--------------------------#
 # Extract Discovered Words #
 #--------------------------#
 if 0 in tasks:
-  datapath = '../data/' 
-  instance_file = datapath + 'flickr30k/bboxes.mat'
-  alignment_file = datapath + 'flickr30k/flickr30k_phrases.txt'
-  caption_file = datapath + 'flickr30k/results_20130124.token'
-  phone_corpus = datapath + 'flickr30k/phoneme_level/src_flickr30k.txt'
-  category_file = 'vgg16_model/imagenet_class_index.json'
-
-  preproc = Flickr_Preprocessor(instance_file, alignment_file, caption_file, category_file=category_file, image_path='../../data/flickr30k/flickr30k-images/')
-  model_names = ['mixture', 'hmm', 'nmt-novt', 'nmt-novc']
-  pred_alignment_files = ['../smt/exp/ibm1_phoneme_level_clustering/flickr30k_pred_alignment.json', '../hmm/exp/aug_31_flickr/flickr30k_pred_alignment.json', '../nmt/exp/feb26_normalize_over_time/output/alignment.json', '../nmt/exp/feb28_phoneme_level_clustering/output/alignment.json']
+  if args.dataset == 'mscoco2k' or args.dataset == 'mscoco20k':
+    datapath = 'data/' + args.dataset
+    phone_corpus = datapath + '%s_phone_captions.txt' % args.dataset
+    gold_alignment_file = data_path + '%s_gold_alignments.json' % args.dataset
+    with open(args.exp_dir+'model_names.txt', 'r') as f:
+      model_names = f.read().strip().split()
+    pred_alignment_files = ['%s_pred_alignment.json' % model_name for model_name in model_names]
+  elif args.dataset == 'flickr':
+    datapath = '../data/flickr30k/'
+    # TODO Make this more general 
+    phone_corpus = datapath + 'phoneme_level/src_flickr30k.txt'
+    gold_alignment_file = data_path + 'phoneme_level/%s_gold_alignments.json' % args.dataset
+    model_names = ['mixture', 'hmm', 'nmt-novt', 'nmt-novc']
+    pred_alignment_files = ['../smt/exp/ibm1_phoneme_level_clustering/flickr30k_pred_alignment.json', '../hmm/exp/aug_31_flickr/flickr30k_pred_alignment.json', '../nmt/exp/feb26_normalize_over_time/output/alignment.json', '../nmt/exp/feb28_phoneme_level_clustering/output/alignment.json']
     
   for i, (model_name, pred_alignment_file) in enumerate(zip(model_names, pred_alignment_files)):
     discovered_word_file = 'tdev2/WDE/share/discovered_words_%s.class' % model_name
-    preproc.alignment_to_word_classes(pred_alignment_file, phone_corpus, discovered_word_file)
+    alignment_to_word_classes(pred_alignment_file, phone_corpus, discovered_word_file)
+    alignment_to_word_unit
 
 #------------------------#
 # Phone-level Evaluation #
@@ -35,10 +45,10 @@ if 0 in tasks:
 if 1 in tasks:
   wrd_path = pkg_resources.resource_filename(
               pkg_resources.Requirement.parse('WDE'),
-                          'WDE/share/flickr30k_word_units.wrd')
+                          'WDE/share/%s_word_units.wrd' % args.dataset)
   phn_path = pkg_resources.resource_filename(
               pkg_resources.Requirement.parse('WDE'),
-                          'WDE/share/flickr30k_phone_units.phn')
+                          'WDE/share/%s_phone_units.phn' % args.dataset)
 
   gold = Gold(wrd_path=wrd_path, 
                 phn_path=phn_path) 
